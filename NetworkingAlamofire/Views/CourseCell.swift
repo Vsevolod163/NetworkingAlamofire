c1d0eddb3535a297d40d52a928d0f1b9969af647
Vsevolod <vsevolod.lashin@gmail.com>
2023-05-14 20:07:24 +0300

    fetchData function added

--- NetworkingAlamofire/Views/CourseCell.swift
+++ NetworkingAlamofire/Views/CourseCell.swift
@@ -13,10 +13,21 @@ final class CourseCell: UITableViewCell {
     @IBOutlet var numberOfLessons: UILabel!
     @IBOutlet var numberOfTests: UILabel!
     
+    private let networkManager = NetworkManager.shared
+    
     func configure(with course: Course) {
         courseNameLabel.text = course.name
         numberOfLessons.text = "Number of lessons: \(course.numberOfLessons)"
         numberOfTests.text = "Number of tests: \(course.numberOfTests)"
+        
+        networkManager.fetchData(from: course.imageUrl) { [weak self] result in
+            switch result {
+            case .success(let imageData):
+                self?.courseImage.image = UIImage(data: imageData)
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 
 }

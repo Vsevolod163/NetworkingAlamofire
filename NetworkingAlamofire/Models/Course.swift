1b25a6495026eee848897243ca9c68978778b04b
Vsevolod <vsevolod.lashin@gmail.com>
2023-05-15 23:53:06 +0300

    CourseAdapter struct added

--- NetworkingAlamofire/Models/Course.swift
+++ NetworkingAlamofire/Models/Course.swift
@@ -27,8 +27,22 @@ struct Course: Codable {
         numberOfTests = courseData["number_of_tests"] as? Int ?? 0
     }
     
+    init(from courseAdapter: CourseAdapter) {
+        name = courseAdapter.name
+        imageUrl = courseAdapter.imageUrl
+        numberOfLessons = Int(courseAdapter.numberOfLessons) ?? 0
+        numberOfTests = Int(courseAdapter.numberOfTests) ?? 0
+    }
+    
     static func getCourses(from value: Any) -> [Course] {
         guard let coursesData = value as? [[String: Any]] else { return [] }
         return coursesData.map { Course(from: $0) }
     }
+}
+
+struct CourseAdapter: Decodable {
+    let name: String
+    let imageUrl: String
+    let numberOfLessons: String
+    let numberOfTests: String
 }
